354068eb550b967af9c76f8aba56783330ece8a7
Nandana-GHO <nandana@gho.care>
2026-03-24 16:43:59 +0530

    completed the UI of profile section

--- src/app/pages/profile/components/perfomance/perfomance.ts
+++ src/app/pages/profile/components/perfomance/perfomance.ts
@@ -1,10 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-perfomance',
-  imports: [MatDivider],
+  imports: [MatDivider,CommonModule],
   templateUrl: './perfomance.html',
   styleUrl: './perfomance.css',
 })
-export class Perfomance {}
+export class Perfomance {
+  stats = [
+  {
+    icon: 'mic',
+    label: 'Shows Hosted: 120'
+  },
+  {
+    icon: 'headphones',
+    label: 'Listener Engagement: 85%',
+    trendIcon: 'trending_up',
+    trendColor: 'text-success'
+  },
+  {
+    icon: 'favorite',
+    label: 'Favourites: 1450'
+  },
+  {
+    icon: 'call',
+    label: 'Calls Received: 310'
+  }
+];
+}

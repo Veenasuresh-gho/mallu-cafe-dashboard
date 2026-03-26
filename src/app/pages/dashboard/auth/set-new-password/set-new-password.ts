7e823924a7b0e7534a1cfb488d9e07fbf9cb21c0
Harikrishnan-gho <harikrishnan@gho.care>
2026-03-26 12:33:16 +0530

    forgot-password-section

---
+++ src/app/pages/dashboard/auth/set-new-password/set-new-password.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-set-new-password',
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule,MatIconModule],
+  templateUrl: './set-new-password.html',
+  styleUrl: './set-new-password.css',
+})
+export class SetNewPassword {
+  passwordForm: FormGroup;
+  submitted = false;
+  hidePassword = true;
+  hideConfirm = true;
+
+  constructor(private fb: FormBuilder) {
+    this.passwordForm = this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  resetPassword() {
+    this.submitted = true;
+
+    if (this.passwordForm.invalid) return;
+
+    const password = this.passwordForm.get('password')?.value;
+    const confirmPassword = this.passwordForm.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
+
+    console.log('Password reset:', password);
+    // Call your API to reset password here
+  }
+}

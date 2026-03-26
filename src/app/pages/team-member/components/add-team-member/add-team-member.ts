e65fd4dec2add5ed2fa99457a31f7cc888a235fc
Nandana-GHO <nandana@gho.care>
2026-03-26 16:38:01 +0530

    completed add member ui

--- src/app/pages/team-member/components/add-team-member/add-team-member.ts
+++ src/app/pages/team-member/components/add-team-member/add-team-member.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatOption, MatSelect, MatSelectTrigger } from '@angular/material/select';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-add-team-member',
-  imports: [MatDialogContent,MatDialogActions,MatDialogClose,
-    MatFormField ,MatLabel,MatSelect,MatOption,MatSelectTrigger,MatChipsModule,
+  imports: [MatDialogContent,MatDialogActions,MatDialogClose,FormsModule,CommonModule,
+    MatFormField ,MatLabel,MatSelect,MatOption,MatSelectTrigger,MatChipsModule,MatSlideToggle
   ],
   templateUrl: './add-team-member.html',
   styleUrl: './add-team-member.css',
@@ -26,6 +29,21 @@ programs = [
 ];
 
 selectedPrograms: any[] = [];
+
+permissions = [
+  { name: 'Media Upload', checked: false },
+  { name: 'Ad Management', checked: false },
+  { name: 'Program Management', checked: false },
+  { name: 'Member Management', checked: false }
+];
+
+isFullAccess = false;
+
+onToggleChange() {
+  this.permissions.forEach(p => {
+    p.checked = this.isFullAccess;
+  });
+}
 
 remove(item: any) {
   this.selectedPrograms = this.selectedPrograms.filter(p => p !== item);

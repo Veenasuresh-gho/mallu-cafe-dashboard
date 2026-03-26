a956f59b63e820cecf6590e4e08c9fe778863093
unknown <veena@gho.care>
2026-03-26 16:33:17 +0530

    team-members

--- src/app/pages/team-member/team-member.ts
+++ src/app/pages/team-member/team-member.ts
@@ -12,113 +12,89 @@ import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-team-member',
-  imports: [MatPaginatorModule, MatTableModule, CommonModule, MatIconModule, MatInputModule, MatSelectModule,FormsModule],
+  imports: [MatPaginatorModule, MatTableModule, CommonModule, MatIconModule, MatInputModule, MatSelectModule, FormsModule],
   templateUrl: './team-member.html',
   styleUrl: './team-member.css',
 })
 export class TeamMember {
 
-  constructor(private dialog: MatDialog) {} 
+  constructor(private dialog: MatDialog) { }
 
   openModal() {
     this.dialog.open(AddTeamMember, {
-       width: '800px',
+      width: '800px',
       disableClose: true
     });
   }
 
   searchText = '';
-  status = 'all';
-  category = 'all';
-  host = 'all';
-  program = 'all';
+  roles = '';
+  programs = '';
 
   columns: string[] = [
-    'program',
-    'category',
-    'host',
-    'duration',
-    'dayTime',
-    'interaction'
+    'team-members',
+    'role',
+    'assignedPrograms',
+    'mobile',
+    'email'
   ];
 
   dataSource = [
     {
-      name: 'Om Shanti Om',
+      name: 'Anjali',
       avatar: '/main/rj1.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Anjali',
-      duration: '1 hr',
-      dayTime: 'Mon - Fri, 9 - 10 am',
-      interaction: 'Allow Calls',
-      interactionClass: 'allow'
+      role: "RJ",
+      assignedPrograms: "Om Shanti Om, Studio Conversations",
+      mobile: "+1 202 555 0143",
+      email: "rjanjali@gmail.com"
     },
     {
-      name: 'Bollywood Rewind',
+      name: 'Priyanka',
       avatar: '/main/rj2.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Priyanka',
-      duration: '2 hr',
-      dayTime: 'Mon - Fri, 10 am - 12 pm',
-      interaction: 'Disabled Calls',
-      interactionClass: 'disabled'
+      role: "RJ",
+      assignedPrograms: "Bollywood Rewind",
+      mobile: "+1 212 555 0187",
+      email: "rjpriyanka@gmail.com"
     },
     {
-      name: 'Hungama Radio',
+      name: 'Shijo',
       avatar: '/main/user-image.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Shijo',
-      duration: '2 hr',
-      dayTime: 'Sat - Sun, 12 - 2 pm',
-      interaction: 'Allow Calls',
-      interactionClass: 'allow'
+      role: "RJ",
+      assignedPrograms: "Hungama Radio",
+      mobile: "+1 305 555 0129",
+      email: "rjshijo@gmail.com"
     },
     {
-      name: 'Indo American News',
+      name: 'Reeva',
       avatar: '/main/rj3.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Reeva',
-      duration: '3 hr',
-      dayTime: 'Mon - Fri, 2 - 5 pm',
-      interaction: 'Allow Calls',
-      interactionClass: 'allow'
+      role: "RJ",
+      assignedPrograms: "Indo American News",
+      mobile: "+1 415 555 0192",
+      email: "rjreeva@gmail.com"
     },
     {
-      name: 'Talk with Stars',
+      name: 'Neena',
       avatar: '/main/rj4.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Neena',
-      duration: '3 hr',
-      dayTime: 'Fri, 5 - 8 pm',
-      interaction: 'Disabled Calls',
-      interactionClass: 'disabled'
+      role: "RJ",
+      assignedPrograms: "Talk with Stars",
+      mobile: "+1 646 555 0135",
+      email: "rjneena@gmail.com"
     },
     {
-      name: 'Studio Conversations',
-      avatar: '/main/rj1.png',
-      category: 'Podcast',
-      categoryClass: 'podcast',
-      host: 'RJ Anjali',
-      duration: '1 hr',
-      dayTime: 'Mon - Fri, 8 - 9 pm',
-      interaction: 'Disabled Calls',
-      interactionClass: 'disabled'
+      name: 'Shibi Roy',
+      avatar: '/main/rj5.png',
+      role: "RJ",
+      assignedPrograms: "Morning Masala, Chai & Chat",
+      mobile: "+1 408 555 0116",
+      email: "rjshibi@gmail.com"
     },
     {
-      name: 'Dial In & Speak Out',
-      avatar: '/main/no-image.png',
-      category: 'Live',
-      categoryClass: 'live',
-      host: 'RJ Ashwin',
-      duration: '1 hr 30 min',
-      dayTime: 'Sat - Sun, 9 - 10:30 pm',
-      interaction: 'Allow Calls',
-      interactionClass: 'allow'
+      name: 'Resh',
+      avatar: '/main/rj6.png',
+      role: "RJ",
+      assignedPrograms: "Retro Beats",
+      mobile: "+1 503 555 0158",
+      email:"rjresh@gmail.com"
     }
   ];
 }

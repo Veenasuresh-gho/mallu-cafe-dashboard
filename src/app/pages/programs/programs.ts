b227c377331406471870e309f86f9b1046fa41d6
Veenasuresh-gho <veena@gho.care>
2026-03-30 15:45:46 +0530

    ui changes

--- src/app/pages/programs/programs.ts
+++ src/app/pages/programs/programs.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,7 +10,10 @@ import { AddNewProgram } from './components/add-new-program/add-new-program';
 import { MatDialog } from '@angular/material/dialog';
 import { PrimaryButton } from '../../components/primary-button/primary-button';
 import { SelectDropDown } from '../../components/select-drop-down/select-drop-down';
-
+import { ghoresult, tags } from '../../../model/ghomodel';
+import { GHOService } from '../../services/ghosrvs';
+import { GHOUtitity } from '../../services/utilities';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-programs',
   standalone: true,
@@ -18,18 +21,79 @@ import { SelectDropDown } from '../../components/select-drop-down/select-drop-do
   templateUrl: './programs.html',
   styleUrl: './programs.css',
 })
-export class Programs {
+export class Programs implements OnInit,AfterViewInit {
 
   constructor(private dialog: MatDialog) { }
 
-openModal() {
-  this.dialog.open(AddNewProgram, {
-    width: '90%',
-    maxWidth: '600px',
-    maxHeight: '95vh',
-    disableClose: true
-  });
+  openModal() {
+    this.dialog.open(AddNewProgram, {
+      width: '90%',
+      maxWidth: '600px',
+      maxHeight: '95vh',
+      disableClose: true
+    });
+  }
+
+  ngOnInit(): void {
+    this.getProgramList();
+  }
+
+  srv = inject(GHOService);
+  utl = inject(GHOUtitity);
+  tv: tags[] = [];
+  res: ghoresult = new ghoresult();
+
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  dataSource = new MatTableDataSource<any>([]);
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+getProgramList(): void {
+  this.tv = [{ T: 'c10', V: '3' }];
+
+  this.srv.getdata('program', this.tv)
+    .subscribe({
+      next: (r) => {
+        const rawData = r.Data?.[0] || [];
+
+        this.dataSource.data = rawData.map((item: any) => ({
+          name: item.Title,
+          avatar: '/main/no-image.png',
+          category: item.CategoryName,
+          categoryClass:
+            item.CategoryName === 'Podcast'
+              ? 'podcast'
+              : item.CategoryName === 'Live'
+              ? 'live'
+              : 'pre',
+          host: item.HostName,
+          duration: item.Duration,
+          dayTime: `${item.DayRange}, ${item.TimeRange}`,
+          interaction: item.IsCallAllowed
+            ? 'Allow Calls'
+            : 'Disabled Calls',
+          interactionClass: item.IsCallAllowed
+            ? 'allow'
+            : 'disabled'
+        }));
+
+        setTimeout(() => {
+          this.dataSource.paginator = this.paginator;
+        });
+      },
+      error: (err) => {
+        console.error('API Error:', err);
+      }
+    });
 }
+
+  get showPaginator(): boolean {
+    return this.dataSource.data.length > 7;
+  }
+
   searchText = '';
   status = 'all';
   category = 'all';
@@ -45,83 +109,4 @@ openModal() {
     'interaction'
   ];
 
-  dataSource = [
-    {
-      name: 'Om Shanti Om',
-      avatar: '/main/rj1.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Anjali',
-      duration: '1 hr',
-      dayTime: 'Mon - Fri, 9 - 10 am',
-      interaction: 'Allow Calls',
-      interactionClass: 'allow'
-    },
-    {
-      name: 'Bollywood Rewind',
-      avatar: '/main/rj2.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Priyanka',
-      duration: '2 hr',
-      dayTime: 'Mon - Fri, 10 am - 12 pm',
-      interaction: 'Disabled Calls',
-      interactionClass: 'disabled'
-    },
-    {
-      name: 'Hungama Radio',
-      avatar: '/main/user-image.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Shijo',
-      duration: '2 hr',
-      dayTime: 'Sat - Sun, 12 - 2 pm',
-      interaction: 'Allow Calls',
-      interactionClass: 'allow'
-    },
-    {
-      name: 'Indo American News',
-      avatar: '/main/rj3.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Reeva',
-      duration: '3 hr',
-      dayTime: 'Mon - Fri, 2 - 5 pm',
-      interaction: 'Allow Calls',
-      interactionClass: 'allow'
-    },
-    {
-      name: 'Talk with Stars',
-      avatar: '/main/rj4.png',
-      category: 'Pre-Scheduled',
-      categoryClass: 'pre',
-      host: 'RJ Neena',
-      duration: '3 hr',
-      dayTime: 'Fri, 5 - 8 pm',
-      interaction: 'Disabled Calls',
-      interactionClass: 'disabled'
-    },
-    {
-      name: 'Studio Conversations',
-      avatar: '/main/rj1.png',
-      category: 'Podcast',
-      categoryClass: 'podcast',
-      host: 'RJ Anjali',
-      duration: '1 hr',
-      dayTime: 'Mon - Fri, 8 - 9 pm',
-      interaction: 'Disabled Calls',
-      interactionClass: 'disabled'
-    },
-    {
-      name: 'Dial In & Speak Out',
-      avatar: '/main/no-image.png',
-      category: 'Live',
-      categoryClass: 'live',
-      host: 'RJ Ashwin',
-      duration: '1 hr 30 min',
-      dayTime: 'Sat - Sun, 9 - 10:30 pm',
-      interaction: 'Allow Calls',
-      interactionClass: 'allow'
-    }
-  ];
 }

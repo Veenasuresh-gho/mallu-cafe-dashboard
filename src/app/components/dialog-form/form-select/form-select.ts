9f29f742898a1bbc554d24a997fe1eca418f20c8
Harikrishnan-gho <harikrishnan@gho.care>
2026-03-30 17:24:31 +0530

    add-custom-calendar

--- src/app/components/dialog-form/form-select/form-select.ts
+++ src/app/components/dialog-form/form-select/form-select.ts
@@ -1,18 +1,48 @@
+// import { CommonModule } from '@angular/common';
+// import { Component, Input } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-form-select',
+//    standalone: true,
+//   imports: [CommonModule, FormsModule],
+//   templateUrl: './form-select.html',
+//   styleUrl: './form-select.css',
+// })
+// export class FormSelect {
+//     @Input() label: string = '';
+//   @Input() options: string[] = [];
+//  @Input() model: string = '';
+//    @Input() name: string = '';
+//     @Input() placeholder: string = '';
+// }
+
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-select',
-   standalone: true,
+  standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './form-select.html',
   styleUrl: './form-select.css',
 })
 export class FormSelect {
-    @Input() label: string = '';
+  @Input() label: string = '';
   @Input() options: string[] = [];
- @Input() model: string = '';
-   @Input() name: string = '';
-    @Input() placeholder: string = '';
+  @Input() model: string = '';
+  @Input() name: string = '';
+  @Input() placeholder: string = '';
+
+  open = false;
+
+  toggleDropdown() {
+    this.open = !this.open;
+  }
+
+  select(value: string) {
+    this.model = value;
+    this.open = false;
+  }
 }
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TodayScheduleSection } from './today-schedule-section/today-schedule-section';
 import { MusicPlayer } from './music-player/music-player';
 import { Summary } from './summary/summary';
+import { CustomCalendar } from '../../components/custom-calendar/custom-calendar';
 
 @Component({
   selector: 'app-dashboard',

f2f3a88116f1c52b8ddf8f34a84c08d4c07de68b
unknown <veena@gho.care>
2026-04-01 17:17:08 +0530

    toast

--- src/app/components/primary-button/primary-button.ts
+++ src/app/components/primary-button/primary-button.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-primary-button',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, CommonModule],
+  imports: [MatIconModule, MatButtonModule, CommonModule, MatProgressSpinnerModule],
   templateUrl: './primary-button.html',
   styleUrl: './primary-button.css',
 })
